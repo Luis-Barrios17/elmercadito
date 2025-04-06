7301280989ba704f5fd829c07370ba24fac48d6e
LuisR <up220110@alumnos.upa.edu.mx>
2025-04-06 15:41:36 -0600

    proyecto4

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['tienditasistemas1-app-e9752b9ed618.herokuapp.com'], // Agrega aquí los dominios permitidos
+    },
+  };
+  
+  export default nextConfig;
