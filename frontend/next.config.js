fdfee55872138af1e06594c94cfda3d2a9d65aa4
LuisR <up220110@alumnos.upa.edu.mx>
2025-04-06 19:09:38 -0600

    2345678

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['tienditasistemas1-app-e9752b9ed618.herokuapp.com'], // Agrega aquí los dominios permitidos
-  },
-};
-
-export default nextConfig;
+    images: {
+      domains: [
+        'tienditasistemas1-app-e9752b9ed618.herokuapp.com', // Dominio del backend para cargar imágenes
+        // Agrega aquí otros dominios si necesitas cargar imágenes de servicios externos
+      ],
+    },
+  };
+  
+  export default nextConfig;
